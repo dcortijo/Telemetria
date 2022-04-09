3fc39aac262dc19f6d98a39949d98e9c15de1638
Daniel Cortijo <dcortijo@ucm.es>
2022-04-09 10:23:52 +0200

    Puesto como en el documento los enums
    Aunque tendrán que ser ints para que sean más generales y los casteamos a enum ee el juego? no lo sé

--- Telemetria/Telemetria/EventData.h
+++ Telemetria/Telemetria/EventData.h
@@ -3,10 +3,24 @@
 enum EventType {
 	SessionStart,
 	SessionEnd,
-	CombatStart,
-	CombatEnd,
+	RoundStart,
+	RoundEnd,
 	Attack,
 	Ability,
-	TakeHit,
+	ReceiveDamage,
 	ERROR
+};
+
+enum Players {
+	PLAYER1, PLAYER2
+};
+
+enum Characters {
+	MKWH00P, F10R, Mockingbird
+};
+
+enum Attacks {
+};
+
+enum Abilities {
 };
